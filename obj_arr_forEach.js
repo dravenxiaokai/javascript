4ca4d78f6b27502b5005794170273246b3b4a278
Draven <jspankai@vip.qq.com>
2017-08-18 07:08:24 +0800

    custom forEach

--- obj_arr_forEach.js
+++ obj_arr_forEach.js
@@ -8,7 +8,7 @@ function forEach(obj, fn) {
     if (obj instanceof Array) {
         //准确判断是不是数组
         obj.forEach(function (item, index) {
-            fn(index, item)
+            fn(item, index)
         })
     } else {
         //不是数组就是对象
@@ -17,6 +17,11 @@ function forEach(obj, fn) {
         ]
     }
 }
+
+var arr = [1, 2, 3]
+forEach(arr, function (item, index) {
+    console.log(index, item)
+})
 
 var obj = { x: 100, y: 200 }
 forEach(obj, function (key, value) {
