01e4b8954561a5fcb762e5ea1f6eb9069818404b
Draven <jspankai@vip.qq.com>
2017-08-17 21:45:19 +0800

    demo

--- answer.js
+++ answer.js
@@ -16,3 +16,27 @@ for (i = 0; i < 10; i++) {
         document.body.appendChild(a)
     })(i)
 }
+
+/**
+ * 如果理解作用域
+ * 自由变量
+ * 作用域链，即自由变量的查找
+ * 闭包的两个场景
+ */
+//  闭包实际应用中主要用于封装变量，收敛权限
+function isFirstLoad() {
+    var _list = []
+    return function (id) {
+        if (_list.indexOf(id) >= 0) {
+            return false
+        } else {
+            _list.push(id)
+            return true
+        }
+    }
+}
+var firstLoad = isFirstLoad()
+firstLoad(10)   //  true
+firstLoad(10)   //  false
+firstLoad(20)   // true
+firstLoad(20)   //  false
