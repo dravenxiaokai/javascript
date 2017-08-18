8475bcacb0d9022755b9ead43bf031a9ec8f0fb9
Draven <jspankai@vip.qq.com>
2017-08-18 08:57:36 +0800

    update property

--- attribute.js
+++ attribute.js
@@ -4,3 +4,10 @@ p.getAttribute('data-name')
 p.setAttribute('data-name', 'email')
 p.getAttribute('style')
 p.setAttribute('style', 'font-size:30px;')
+
+
+/**
+ * DOM节点的Attribute和property有何区别
+ * property只是一个JS对象的属性的修改
+ * Attribute是对html标签属性的修改
+ */
